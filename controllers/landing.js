c4aba2b3f6184bbd6a27ed387abdd735a74038f2
rozophat <rozophat90@gmailcom>
2020-04-05 22:44:00 +0700

    Using POST for Landing

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,3 +1,8 @@
 exports.get_landing = function(req, res, next) {
   res.render('landing', { title: 'Landing Page' });
 }
+
+exports.submit_lead = function(req, res, next) {
+  console.log("Lead email: ", req.body.lead_email);
+  res.redirect('/landing');
+} 
