189e7b631a9b2bab337841eaa9551aef140abe7c
rozophat <rozophat90@gmailcom>
2020-04-06 22:37:26 +0700

    Create Authentication Page

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 let index = require('../controllers/index');
+let user = require('../controllers/user');
 
+router.get('/login', user.show_login);
+router.get('/signup', user.show_signup);
 /* GET home page. */
 router.get('/', index.index);
 
