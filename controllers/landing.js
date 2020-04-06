40d0b2320ea4d113e6ee5b9943ed3fd5c71d6c48
rozophat <rozophat90@gmailcom>
2020-04-06 23:39:40 +0700

    Create Authentication function

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,7 +1,7 @@
 const models = require('../models')
 
 exports.get_landing = function(req, res, next) {
-  res.render('landing', { title: 'Landing Page' });
+  res.render('landing', { title: 'Landing Page', user: req.user });
 }
 
 exports.submit_lead = function(req, res, next) {
--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,9 @@
+let models = require("../models");
+let bcrypt = require("bcrypt");
+const passport = require("passport");
+const myPassport = require("../passport_setup")(passport); 
+let flash = require("connect-flash");
+
 exports.show_login = function(req, res, next) {
     res.render('user/login', { formData: {}, errors:{} });
   }
@@ -7,9 +13,33 @@ exports.show_signup = function(req, res, next) {
 }
 
 exports.login = function(req, res, next) {
-  res.render('user/signup', { formData: {}, errors:{} });
+  passport.authenticate('local',{
+    successRedirect: "/landing",
+    failureRedirect: "/login",
+    failureFlash: true
+  })(req, res, next);
+}
+
+const generateHash = function(password){
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
 }
 
 exports.signup = function(req, res, next) {
-  res.render('user/signup', { formData: {}, errors:{} });
+  const newUser = models.User.build({
+    email: req.body.email,
+    password: generateHash(req.body.password)
+  });
+  return newUser.save().then(result =>{
+    passport.authenticate('local',{
+      successRedirect: "/landing",
+      failureRedirect: "/signup",
+      failureFlash: true
+    })(req, res, next);
+  })
+}
+
+exports.logout = function(req, res, next) {
+  req.logout();
+  req.session.destroy();
+  res.redirect('/landing');
 }
