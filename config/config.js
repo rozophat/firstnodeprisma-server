c52adddf2df4cad6b530d700b36af0caf1494fb9
rozophat <rozophat90@gmailcom>
2020-04-08 22:53:38 +0700

    change database config

--- config/config.js
+++ config/config.js
@@ -1,12 +1,11 @@
 module.exports = {
   "development": {
-    "username": "express-mvp-dbuser",
-    "password": '123456',
-    "database": "express-mvp-db",
-    "host": "127.0.0.1",
+    "username": "kzjoifhzluvynr",
+    "password": 'e86c7b6aa4b93149243ac997f0ebb460c9e7ed3d117ddaa91d1a75e698a4f44a',
+    "database": "d9m38cf0pnss8o",
+    "host": "ec2-52-71-85-210.compute-1.amazonaws.com",
     "dialect": "postgres",
-    "operatorsAliases": false,
-    "port": 5432
+    "port": 5432,
   },
   "test": {
     "username": "root",
