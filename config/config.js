9ed5e8c6a40a8c9dcdbf65c4845443ed01f4fcd5
rozophat <rozophat90@gmailcom>
2020-04-10 15:44:52 +0700

    change to elephant cloud

--- config/config.js
+++ config/config.js
@@ -1,18 +1,18 @@
 module.exports = {
   "development": {
-    "username": "kzjoifhzluvynr",
-    "password": 'e86c7b6aa4b93149243ac997f0ebb460c9e7ed3d117ddaa91d1a75e698a4f44a',
-    "database": "d9m38cf0pnss8o",
-    "host": "ec2-52-71-85-210.compute-1.amazonaws.com",
-    "dialect": "postgres",
-    "port": 5432,
-
-    // "username": "vfddtytj",
-    // "password": 'sR6XGimnBX0MsXOP8lZpKfl_MtQMcN4x',
-    // "database": "vfddtytj",
-    // "host": "satao.db.elephantsql.com",
+    // "username": "kzjoifhzluvynr",
+    // "password": 'e86c7b6aa4b93149243ac997f0ebb460c9e7ed3d117ddaa91d1a75e698a4f44a',
+    // "database": "d9m38cf0pnss8o",
+    // "host": "ec2-52-71-85-210.compute-1.amazonaws.com",
     // "dialect": "postgres",
     // "port": 5432,
+
+    "username": "vfddtytj",
+    "password": 'sR6XGimnBX0MsXOP8lZpKfl_MtQMcN4x',
+    "database": "vfddtytj",
+    "host": "satao.db.elephantsql.com",
+    "dialect": "postgres",
+    "port": 5432,
 
 
     // "ssl":true,
