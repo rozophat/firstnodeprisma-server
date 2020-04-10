3cc9bbd03abe11441b0a4a13211b983dbae1d9d6
rozophat <rozophat90@gmailcom>
2020-04-10 15:41:24 +0700

    change to heroku db

--- models/index.js
+++ models/index.js
@@ -46,11 +46,11 @@ const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+// }
 
 db['Author'] = sequelize['import'](path.join(__dirname, './Author.js'));
 db['Book'] = sequelize['import'](path.join(__dirname, './Book.js'));
